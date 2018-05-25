8447bf47b59d4fe0e665acaa9284100f921c1670
Anthony <anthony.a.xu@hotmail.com>
2018-05-25 23:39:56 +1000

    finished PQ.c

--- PQ.c
+++ PQ.c
@@ -5,36 +5,82 @@
 #include <assert.h>
 
 
-struct PQRep {
-
-};
-
+typedef struct PQRep {
+	int size;
+	ItemPQ *item;  // current item
+} PQ;
 
 PQ newPQ() {
-	return NULL;
+	PQ *pq;
+	if (pq = malloc(sizeof(PQ)) == NULL) {
+		fprintf(stderr, "Error!\n", );
+	}
+	int initial_size = 16;
+	pq->item = malloc(sizeof(ItemPQ))*(initial_size + 1)
+	pq->size = 0;
+	return pq;
 }
 
-int PQEmpty(PQ p) {
-		return 0;
+int PQEmpty(PQ pq) {
+	return (pq->size == 0);
 }
 
 void addPQ(PQ pq, ItemPQ element) {
-
+	int i = pq->size + 1;
+	ItemPQ *item = pq->item;
+	item[i] = element;
+	while ((item[i]->value > item[i/2]) && (i > 1)) {
+		ItemPQ *tmp;
+		tmp = item[i/2];
+		item[i/2] = item[i];
+		item[i] = tmp;
+		i = i/2;
+	}
+	pq->size++;
 }
 
 ItemPQ dequeuePQ(PQ pq) {
-	ItemPQ throwAway = {0};
-	return throwAway;
+	ItemPQ *item = pq->item;
+	ItemPQ *tmp = item[1];
+	item[1] = item[pq->size];
+	updatePQ(pq, item);
+	return tmp;
 }
 
 void updatePQ(PQ pq, ItemPQ element) {
-
+	int index = 1;  // (2*1)
+	int current;
+	ItemPQ *tmp;
+	while (current < pq->size) {
+		current = 2*index;
+		if (item[current] < item[current + 1]) {
+			current++;
+		}
+		if (item[current]->value < item[index]->value) {
+			tmp = item[current];
+			item[current] = item[index];
+			item[index] = tmp;
+			index = current;
+		} else {
+			break;
+		}
+	}
 }
 
 void  showPQ(PQ pq) {
-
+	printf("Value in priority queue:");
+	for (int i = 1; i <= pq->size; i++) {
+		printf("%d ", pq->item[i]->value);
+	}
+	printf("\n");
+	printf("Key in priority queue:");
+	for (int i = 1; i <= pq->size; i++) {
+		printf("%d ", pq->item[i]->key);
+	}
+	printf("\n");
 }
 
 void  freePQ(PQ pq) {
-
+	free(pq->item);
+	free(pq);
 }
