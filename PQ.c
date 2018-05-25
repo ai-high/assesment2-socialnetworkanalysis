a5ce18348ed2ab4ea906182c5c1dcf3f22c250db
Anthony <anthony.a.xu@hotmail.com>
2018-05-26 04:35:47 +1000

    completed but not fully in descending order

--- PQ.c
+++ PQ.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <time.h>
 
 typedef struct PQRep {
 	int size;
 	ItemPQ *item;
 } PQRep;
+
+// yeah nah
 
 PQ newPQ() {
 	PQ pq;
@@ -28,7 +31,7 @@ void addPQ(PQ pq, ItemPQ element) {
 	int i = pq->size + 1;
 	ItemPQ *item = pq->item;
 	item[i] = element;
-	while ((item[i].value > item[i/2].value) && (i > 1)) {
+	while ((item[i].value < item[i/2].value) && (i > 1)) {
 		ItemPQ tmp;
 		tmp = item[i/2];
 		item[i/2] = item[i];
@@ -54,7 +57,7 @@ void updatePQ(PQ pq, ItemPQ element) {
 	// ItemPQ *item = element;
 	while (current < pq->size) {
 		current = 2*index;
-		if (pq->item[current].value < pq->item[current + 1].value) {
+		if (pq->item[current].value > pq->item[current + 1].value) {
 			current++;
 		}
 		if (pq->item[current].value < pq->item[index].value) {
@@ -70,19 +73,97 @@ void updatePQ(PQ pq, ItemPQ element) {
 
 void  showPQ(PQ pq) {
 	printf("Value in priority queue: ");
-	int i;
-	for (i = 1; i <= pq->size; i++) {
+	for (int i = 1; i <= pq->size; i++) {
 		printf("%d ", pq->item[i].value);
 	}
 	printf("\n");
-	printf("Key in priority queue: ");
-	for (i = 1; i <= pq->size; i++) {
-		printf("%d ", pq->item[i].key);
-	}
+	// printf("Key in priority queue: ");
+	// for (int i = 1; i <= pq->size; i++) {
+	// 	printf("%d ", pq->item[i].key);
+	// }
 	printf("\n");
 }
 
 void  freePQ(PQ pq) {
 	free(pq->item);
 	free(pq);
+}
+
+int main(void) {
+    int i;
+    PQ pq = newPQ();
+    ItemPQ new;
+    srand(time(NULL));
+    for (i = 1; i < 9; i++) {
+        new.value = rand()%10000;
+        printf("add node with value: %d\n", new.value);
+        addPQ(pq, new);
+    }
+    showPQ(pq);
+    printf("\ndequeue all values:\n");
+    for (i = 1; i < 9; i++) {
+        new = dequeuePQ(pq);
+        printf("remove node with value: %d, queue size after removal: %d\n", new.value, pq->size);
+        showPQ(pq);
+    }
+    freePQ(pq);
+
+    // PQ pq = newPQ();
+    // ItemPQ new;
+    // srand(time(NULL));
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // // printf("second: %d\n\n", pq->item[pq->size/2].value);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // // printf("second: %d\n\n", pq->item[pq->size/2].value);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // // printf("second: %d\n\n", pq->item[pq->size/2].value);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // // printf("second: %d\n\n", pq->item[pq->size/2].value);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+    // // printf("second: %d\n\n", pq->item[pq->size/2].value);
+    // new.value = rand()%100;
+    // printf("adding %d\n", new.value);
+    // addPQ(pq, new);
+
+    // showPQ(pq);
+
+    // ItemPQ remove = dequeuePQ(pq);
+    // printf("removed %d\n", remove.value);
+    // showPQ(pq);
+
+    // remove = dequeuePQ(pq);
+    // printf("removed %d\n", remove.value);
+    // showPQ(pq);
+
+    // remove = dequeuePQ(pq);
+    // printf("removed %d\n", remove.value);
+    // showPQ(pq);
+
+    // freePQ(pq);
+    return 0;
 }
