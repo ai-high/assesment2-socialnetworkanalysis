e988bc21b4b76fad0ae881ea010852bcb8b70fe3
Zac <zac.moran@icloud.com>
2018-05-27 07:56:08 +1000

    dijkstras

--- Dijkstra.c
+++ Dijkstra.c
@@ -5,42 +5,31 @@
 #include <assert.h>
 #include <stdio.h>
 #define INFINITY 9999
-#define WTF 25
 
 ShortestPaths init(Graph g, Vertex v);
 
 ShortestPaths dijkstra(Graph g, Vertex v) {
 
 	ShortestPaths sp = init(g,v);
-	//PQ pq = newPQ();
-	for (int i = 0; i < WTF; i++) {
+	
+	PQ pq = newPQ();
+	ItemPQ item;
+	for (int i = 0; i < sp.noNodes; i++) {
 		sp.dist[i] = INFINITY;
-		//Add to PQ
-		//addPQ(pq, ItemPQ);
 		sp.pred[i] = NULL;
+		item.key = i;
+		item.value = INFINITY;
+		addPQ(pq, item);
 	}
 	sp.dist[v] = 0;
-	//update PQ now with v
-	int newDistance;
-	//main loop
-	while(PQEmpty(pq)==0){
-		//get node from PQ
-		curr = dequeuePQ(PQ); //or something to get node
-		//go through neighbours of item
-		for (int i = 0; i < WTF; i++) {
-			if(g->adjListArray[i]!=NULL) {
-				newDistance = sp.dist[curr] + /* distance from curr to i */
-				if (newDistance < sp.dist[i]) {
-					sp.dist[i] = newDistance;
-					sp.pred[i] = /* current node */
-				}
-			}
-
-		}
-
+	item.key = v;
+	item.value = 0;
+	updatePQ(pq, item);
+	showPQ(pq);
+	for (int i = 0; i < sp.noNodes; ++i)
+	{
+		printf("[%d]",sp.dist[i]);
 	}
-
-
 
 	return sp;
 }
@@ -55,16 +44,10 @@ void  freeShortestPaths(ShortestPaths paths) {
 }
 
 ShortestPaths init(Graph g, Vertex v) {
-	ShortestPaths sp;
-	if((g= malloc(sizeof(ShortestPaths))) == NULL){
-		fprintf(stderr, "Error!!  .... \n");
-	}
-	sp.src = v;
-	sp.noNodes = 0;  //increment as we go
-	//sp.dist = malloc(sizeof(int)*g->nVert);
-	//sp.pred = malloc(sizeof(PredNode)*g->nVert);
-	sp.dist = malloc(sizeof(int)*WTF);
-	sp.pred = malloc(sizeof(PredNode)*WTF);
-	return sp;
-
+	ShortestPaths *sp = malloc(sizeof(ShortestPaths));
+	sp->src = v;
+	sp->noNodes = numVerticies(g);
+	sp->dist = malloc(sizeof(int)*numVerticies(g));
+	sp->pred = malloc(sizeof(PredNode)*numVerticies(g));
+	return *sp;
 }
