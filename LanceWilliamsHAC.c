abb3b8d7464b3902e4f893f5ee71e99680a8506c
Anthony <anthony.a.xu@hotmail.com>
2018-05-30 16:49:50 +1000

    calculating initial distance between vertices done

--- LanceWilliamsHAC.c
+++ LanceWilliamsHAC.c
@@ -6,6 +6,69 @@
 #include "Dijkstra.h"
 #define INFINITY 9999
 
+typedef struct CNode *ClusterNode;
+typedef struct CLink *ClusterLink;
+typedef struct CRep *ClusterRep;
+
+typedef struct GraphRep {
+	int nVert;
+	AdjList *adjListArray;
+} GraphRep;
+
+// struct that links clusters
+typedef struct CLink {
+	int *clusters;		// array of index of clusters in link
+	ClusterNode *head;	// source cluster when merging
+	ClusterNode *tail;	// dest cluster when merging
+} CLink;
+
+// struct for cluster
+typedef struct CNode {
+	int index;		
+	int *vertices;	// array of all vertices it is holding
+	ClusterLink *link;
+} CNode;
+
+// struct that holds all clusters
+typedef struct CRep {
+	int size; 	// # of clusters
+	int *ClusterArray;	// array that holds all clusters
+} CRep;
+
+// creation of CRep
+ClusterRep initClusterRep(int nV) {
+	ClusterRep clusterRep;
+	if ((clusterRep = malloc(sizeof(CRep))) == NULL) {
+		fprintf(stderr, "Error!!  .... \n");
+		exit(1);
+	}
+	clusterRep->ClusterArray = malloc(sizeof(int)*(nV));
+	clusterRep->size = 0;
+	return clusterRep;
+}
+
+// creation of cluster node
+ClusterNode initClusterNode(int vertex, int *vertArr) {
+	ClusterNode node;
+	if ((node = malloc(sizeof(CNode))) == NULL) {
+		fprintf(stderr, "Error!!  .... \n");
+		exit(1);
+	}
+	// copies vertices from vertArr into it's own array
+	node->vertices = malloc(sizeof(vertArr));
+	for (int i = 0; i < sizeof(vertArr); i++){
+		node->vertices[i] = vertArr[i];
+	}
+	// initially, index is the vertex
+	node->index = vertex;
+	node->link = NULL;
+	return node;
+}
+
+ClusterNode mergeClusterNodes(int src, int dest) {
+
+}
+
 // initialse a new array to hold distances
 float **initDistanceArray(int nVert) {
 	assert(nVert > 0);
@@ -24,7 +87,6 @@ float **initDistanceArray(int nVert) {
 
 // return edge we are looking for
 AdjList findEdge(Graph g, Vertex src, Vertex dest) {
-	// g IS INCOMPLETE TYPE?????
 	AdjList tmp = g->adjListArray[src];
 	while (tmp != NULL) {
 		if (tmp->w == dest) {
@@ -37,8 +99,8 @@ AdjList findEdge(Graph g, Vertex src, Vertex dest) {
 
 // calculates initial distance
 void initDistance(Graph g, float **dist, int src, int dest) {
-	int weight1 = 0;
-	int weight2 = 0;
+	float weight1 = 0;
+	float weight2 = 0;
 	AdjList tmp;
 	if ((tmp = findEdge(g, src, dest)) != NULL) {
 		weight1 = tmp->weight;
@@ -49,9 +111,17 @@ void initDistance(Graph g, float **dist, int src, int dest) {
 	if ((weight1 == 0) && (weight2 == 0)) {
 		dist[src][dest] = dist[dest][src] = INFINITY;
 	} else {
-		int wt = weight1 + weight2;
+		float wt = weight1 + weight2;
 		float distance = 1/wt;
-		dist[src][dest] = dist[dest][src] = distance;
+		if (weight1 == 0) {
+			dist[dest][src] = distance;
+			dist[src][dest] = INFINITY;
+		} else if (weight2 == 0) {
+			dist[src][dest] = distance;
+			dist[dest][src] = INFINITY;
+		} else {
+			dist[src][dest] = dist[dest][src] = distance;
+		}
 	}
 }
 
@@ -75,7 +145,35 @@ Dendrogram LanceWilliamsHAC(Graph g, int method) {
 			initDistance(g, distanceArr, i, j);
 		}
 	}
+	// code to test if initDistance() works // it does
+	for (int i = 0; i < g->nVert; i++) {
+		for (int j = 0; j < g->nVert; j++) {
+			printf("[%d][%d] = %.4f\n", i, j, distanceArr[i][j]);
+		}
+	}
+	printf("\n");
+												// printf("nVert = %d\n", g->nVert);
+	// Create ClusterRep
+	ClusterRep clusterRep = initClusterRep(g->nVert);
+	int *vertArr = malloc(sizeof(int));
+	int vertex;
+	// Create individual cluster (every vertex)
+	for (int i = 0; i < g->nVert; i++) {
+		vertex = vertArr[0] = g->adjListArray[i]->w;
+												// printf("%d\n", g->adjListArray[i]->w);
+		ClusterNode c = initClusterNode(vertex, vertArr);
+		clusterRep->ClusterArray[i] = i;
+		clusterRep->size++;
+	}
+												// printf("clusterrep size = %d\n", clusterRep->size);
 
+	/*
+					I DONT KNOW WHAT TO DO ABOUT DISTANCES AHHHH
+	*/
+
+	mergeClusterNodes(src, dest);
+
+	// INITIALISE DENDROGRAM
 	Dendrogram d = {0};
 	return d;
 }
